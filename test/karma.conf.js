ca3ea88e6ed40d4e2b54b52913557808083152dd
ishita12 <30434843+ishita12@users.noreply.github.com>
2018-01-29 14:40:45 -0500

    coverage report

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,21 @@ module.exports = function(config) {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
       },
+      coverageReporter: {
+      reporters: [
+        {
+          type: 'text',
+          dir: 'coverage/',
+          file: 'coverage.txt'
+        },
+        {
+          type: 'lcovonly',
+          dir: 'coverage/',
+          subdir: '.'
+        },
+        { type: 'text-summary' }
+      ]
+    },
       ChromeCanaryHeadlessNoSandbox: {
         base: 'ChromeCanaryHeadless',
         flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required'],
